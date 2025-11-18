80c89f7a002d7959d1b783d2d05e8a90de1a9e30
Jakub Górnik <jakub.gornikk@gmail.com>
2025-11-18 22:13:22 +0100

    chore: optimize country select

--- modules/supplier/supplier-form.utils.tsx
+++ modules/supplier/supplier-form.utils.tsx
@@ -1,6 +1,5 @@
 import { SupplierFormSchema } from "./supplier-form.validation";
 import { countries as countryData } from "countries-list";
-import { CircleFlag } from "react-circle-flags";
 
 export const createDefaultSupplierFormData = (
   data?: SupplierFormSchema
@@ -14,10 +13,18 @@ export const createDefaultSupplierFormData = (
   addressPostalCode: data?.addressPostalCode || "",
 });
 
+export function getCountryFlag(countryCode: string) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt(0));
+  return String.fromCodePoint(...codePoints);
+}
+
 export const countryOptions = Object.entries(countryData)
   .map(([code, country]) => ({
     value: code,
     label: country.name,
-    icon: <CircleFlag countryCode={code.toLowerCase()} />,
+    icon: <span className="text-lg leading-none">{getCountryFlag(code)}</span>,
   }))
   .sort((a, b) => a.label.localeCompare(b.label));
