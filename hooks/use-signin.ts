edabbad057437d2ba43bc45b2aa98acbafe8cc35
Jakub Górnik <jakub.gornikk@gmail.com>
2026-01-27 20:23:42 +0100

    feat: add auth notifications

--- hooks/use-signin.ts
+++ hooks/use-signin.ts
@@ -4,27 +4,29 @@ import { useRouter } from "next/navigation";
 import api from "@/lib/axios";
 import { auth } from "@/lib/firebase";
 import { AuthenticationForm } from "@/modules/auth/auth-form.validation";
+import { useNotify } from "./use-notify";
 
 export function useSignIn() {
   const router = useRouter();
+  const { showSuccess, showError } = useNotify();
 
   return useMutation({
     mutationFn: async (data: AuthenticationForm) => {
-      {
-        const userCredential = await signInWithEmailAndPassword(
-          auth,
-          data.email,
-          data.password
-        );
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
 
-        const idToken = await userCredential.user.getIdToken();
+      const idToken = await userCredential.user.getIdToken();
 
-        const response = await api.post("/auth/signin", { idToken });
-        return response.data;
-      }
+      const response = await api.post("/auth/signin", { idToken });
+      return response.data;
     },
     onSuccess: () => {
+      showSuccess("Signed in successfully");
       router.push("/dashboard");
     },
+    onError: (error) => showError(error),
   });
 }
--- hooks/use-signup.ts
+++ hooks/use-signup.ts
@@ -1,13 +1,23 @@
 import api from "@/lib/axios";
+import { ROUTES } from "@/lib/routes";
 import { AuthenticationForm } from "@/modules/auth/auth-form.validation";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useNotify } from "./use-notify";
 
 export function useSignUp() {
+  const router = useRouter();
+  const { showSuccess, showError } = useNotify();
+
   return useMutation({
     mutationFn: async (data: AuthenticationForm) => {
       const response = await api.post("/auth/signup", data);
       return response.data;
     },
-    // TODO: add notifications
+    onSuccess: () => {
+      showSuccess("Account created successfully. Please sign in.");
+      router.push(ROUTES.SIGN_IN);
+    },
+    onError: (error) => showError(error),
   });
 }
